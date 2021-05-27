d654b3e49e46ca5a74653db02763fdb6f5cb57c5
Creature <Creature.Creations@outlook.com>
2021-05-27 11:55:53 -0700

    fixed typo in dropdown.classList.remove in dropdown.js

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,15 +1,14 @@
-    //  When the user clicks on the button, 
-    // toggle between hiding and showing the dropdown content 
+    //  When the user clicks on the menu button toggle between hiding and showing the dropdown content 
     function myFunction() {
         document.getElementById("dropdown").classList.toggle("show");
       }
       
-      // Close the dropdown if the user clicks on the menu button
+      // Close the dropdown if the user clicks outside of the dropdown menu
       window.onclick = function(e) {
           if (!e.target.matches('.dropdownbtn')) {
           var dropdown = document.getElementById("dropdown");
               if (dropdown.classList.contains('show')) {
-              myDropdown.classList.remove('show');
+              dropdown.classList.remove('show');
               }
           }
       }
